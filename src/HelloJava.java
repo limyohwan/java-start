fd0e4a1693e341799fd1a67a3edfa778603bdc3c
Yohwan Lim <dyghks7102@naver.com>
2023-11-28 22:50:06 +0900

    feat: hello java

---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {// 중괄호 사이를 블록이라고 함
+
+    public static void main(String[] args) { // 자바 파일을 실행하면 main 메서드를 찾아 프로그램을 시작함
+        System.out.println("Hello Java");
+    }
+    // 자바는 대소문자를 구분함
+}
---
+++ src/HelloJava2.java
@@ -0,0 +1,8 @@
+public class HelloJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java1");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
+    }
+}
