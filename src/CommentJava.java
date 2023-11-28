4f78c6e6fbb804757b0d822f2af29525d1f7aa58
Yohwan Lim <dyghks7102@naver.com>
2023-11-28 23:10:06 +0900

    feat: 주석과 자바 간단 정리

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java1"); // Hello Java1을 출력
+//        System.out.println("Hello Java2"); 한줄 주석(single line comment)
+
+        /* 다중 주석(multi line comment)
+        System.out.println("Hello Java3");
+        System.out.println("Hello Java4");
+         */
+    }
+}
